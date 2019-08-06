f02b7d6b9d7c1fd690742e6cdbc56e06db62ad71
Rachmann Joubert <rachmannjoubert@gmail.com>
2019-08-06 10:59:04 +0100

    added html boilerplate

--- index.php
+++ index.php
@@ -13,4 +13,16 @@ $sql = 'SELECT * FROM `Plants`';
 $query = $db->query($sql);
 $results = $query->fetchAll();
 
-echo $results ['latin-name'];
+var_dump($results);
+
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="utf-8">
+    <title>Collectors App</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+</head>
+</html>
