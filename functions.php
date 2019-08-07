e763c3a9555bd757cfa7523387fc36d0090e6c0a
Rachmann Joubert <rachmannjoubert@gmail.com>
2019-08-07 15:05:24 +0100

    altered process data function so that it creates the desired table format

--- functions.php
+++ functions.php
@@ -14,42 +14,43 @@ function getData($db)
     return $plants;
 }
 
-function getHeadings($plants) {
-    $plantHeading = '<tr>';
-    foreach ($plants[0] as $key => $value) {
-        $plantHeading .= '<th>' . $key . '</th>';
-    }
-    $plantHeading .= '</tr>';
-    return $plantHeading;
-}
+//function getHeadings($plants) {
+//    $plantHeading = '<tr>';
+//    foreach ($plants[0] as $key => $value) {
+//        $plantHeading .= '<th>' . $key . '</th>';
+//    }
+//    $plantHeading .= '</tr>';
+//    return $plantHeading;
+//}
 
-function getTableBody($plants) {
-    $tableBody = '';
-    foreach ($plants as $plant) {
-        $tableBody .= '<tr>';
-        $tableBody .=
-            '<td>' . $plant['id'] . '</td>' .
-            '<td>' . $plant['common_name'] . '</td>' .
-            '<td>' . $plant['latin_name'] . '</td>' .
-            '<td>' . $plant['identification_image'] . '</td>' .
-            '<td>' . $plant['canopy_level'] . '</td>' .
-            '<td>' . $plant['life_span'] . '</td>';
-        $tableBody .= '</tr>';
-    }
-    return $tableBody;
-}
+//function getTableBody($plants) {
+//    $tableBody = '';
+//    foreach ($plants as $plant) {
+//        $tableBody .= '<tr>';
+//        $tableBody .=
+//            '<td>' . $plant['id'] . '</td>' .
+//            '<td>' . $plant['common_name'] . '</td>' .
+//            '<td>' . $plant['latin_name'] . '</td>' .
+//            '<td>' . $plant['identification_image'] . '</td>' .
+//            '<td>' . $plant['canopy_level'] . '</td>' .
+//            '<td>' . $plant['life_span'] . '</td>';
+//            '<td><img src="' . $plant['identification_image'] . '"></td>';
+//        $tableBody .= '</tr>';
+//    }
+//    return $tableBody;
+//}
 
-function processData($plants) {
+function processData(array $plants):string {
     $plantRow = '';
     foreach ($plants as $plant) {
         $plantRow .=
-            '<div class="plant">' .
-            $plant['id'] .
-            $plant['common_name'] .
-            $plant['latin_name'] .
-            $plant['identification_image'] .
-            $plant['canopy_level'] .
-            $plant['life_span'] . '</div>';
+            '<div class="row"><ul><li>' .
+            $plant['id'] . '</li><li>' .
+            $plant['common_name'] . '</li><li>' .
+            $plant['latin_name'] . '</li><li>' .
+            $plant['identification_image'] . '</li><li>' .
+            $plant['canopy_level'] . '</li><li>' .
+            $plant['life_span'] . '</li></ul></div>';
     }
     return $plantRow;
 }
