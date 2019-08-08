1e4ceda9c89c428325e0c25720ac55644081f7d6
Rachmann Joubert <rachmannjoubert@gmail.com>
2019-08-08 16:00:44 +0100

    structured arrays to make them easier to read

--- test/functionsTest.php
+++ test/functionsTest.php
@@ -14,8 +14,20 @@ class FunctionTest extends TestCase
    public function testProcessData_returnsAString_success() {
 
        $testArray = [
-           ['id' => 1, 'common_name' => 'American Persimmon', 'latin_name' => 'Diospyros virginiana', 'canopy_level' => 'CANOPY LAYER', 'life_span' => 60],
-           ['id' => 2, 'common_name' => 'Nodding Saltbush', 'latin_name' => 'Einadia wutans', 'canopy_level' => 'SHRUB LAYER', 'life_span' => 10]
+           [
+               'id' => 1,
+               'common_name' => 'American Persimmon',
+               'latin_name' => 'Diospyros virginiana',
+               'canopy_level' => 'CANOPY LAYER',
+               'life_span' => 60
+           ],
+           [
+               'id' => 2,
+               'common_name' => 'Nodding Saltbush',
+               'latin_name' => 'Einadia wutans',
+               'canopy_level' => 'SHRUB LAYER',
+               'life_span' => 10
+           ]
        ];
 
        $expectedResult =  '<div class="row"><ul><li>1</li><li>American Persimmon</li><li>Diospyros virginiana</li><li>CANOPY LAYER</li><li>60</li></ul></div><div class="row"><ul><li>2</li><li>Nodding Saltbush</li><li>Einadia wutans</li><li>SHRUB LAYER</li><li>10</li></ul></div>';
@@ -32,7 +44,11 @@ class FunctionTest extends TestCase
     public function testProcessData_MalformedInput() {
 
         $testArray = [
-            'id' => 1, 'common_name' => 'American Persimmon', 'latin_name' => 'Diospyros virginiana', 'canopy_level' => 'CANOPY LAYER', 'life_span' => 60
+            'id' => 1,
+            'common_name' => 'American Persimmon',
+            'latin_name' => 'Diospyros virginiana',
+            'canopy_level' => 'CANOPY LAYER',
+            'life_span' => 60
         ];
 
         $expectedResult =  false;
@@ -78,7 +94,11 @@ class FunctionTest extends TestCase
 
     public function testDisplayImage_returnsAString_success() {
 
-        $testArray = [['identification_image' => './images/american-persimmon.jpg'], ['identification_image' => './images/nodding-saltbush.jpg'], ['identification_image' => './images/chinese-chestnut.jpg']];
+        $testArray = [
+            ['identification_image' => './images/american-persimmon.jpg'],
+            ['identification_image' => './images/nodding-saltbush.jpg'],
+            ['identification_image' => './images/chinese-chestnut.jpg']
+        ];
 
         $expectedResult = '<div><img class ="sourceImage" src="./images/american-persimmon.jpg"></div><div><img class ="sourceImage" src="./images/nodding-saltbush.jpg"></div><div><img class ="sourceImage" src="./images/chinese-chestnut.jpg"></div>';
 
@@ -91,7 +111,11 @@ class FunctionTest extends TestCase
 
     public function testDisplayImage_MalformedTest() {
 
-        $testArray = ['identification_image' => './images/american-persimmon.jpg', 'id' => 5, 'latin_name' => 'Diospyros virginiana'];
+        $testArray = [
+            'identification_image' => './images/american-persimmon.jpg',
+            'id' => 5,
+            'latin_name' => 'Diospyros virginiana'
+        ];
 
         $expectedResult = false;
 
